065626620ec0bf4214dab13b542ee57892c00888
Dylan Guedes <djmgguedes@gmail.com>
2014-08-20 07:23:41 -0300

    Repositorio sem mudanças

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Desafio2MDS::Application.config.secret_token = 'dd492b67eae479f2467ef75049d19a69cb9cfbed2db86e4d00a87b9c2d9fad8a3487c1f40f7fc098d0ba03d1e5b9be725e0e750dc92d99c5333598aa635b2a30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Desafio2MDS::Application.config.session_store :cookie_store, key: '_Desafio2MDS_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Desafio2MDS::Application.config.session_store :active_record_store
